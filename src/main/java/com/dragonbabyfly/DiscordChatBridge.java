a66c3e3453460b7cffee12d98c41fa6221659bde
thdaele <thomas.vandaele2000@gmail.com>
2022-01-10 17:11:31 +0100

    Initial commit

---
+++ src/main/java/com/dragonbabyfly/DiscordChatBridge.java
@@ -0,0 +1,10 @@
+package com.dragonbabyfly;
+
+import net.fabricmc.api.ModInitializer;
+
+public class DiscordChatBridge implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
